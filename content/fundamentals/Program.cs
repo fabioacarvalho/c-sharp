217d1d4e6882eff4ef414127efda72d3eb7a2a45
Fábio Carvalho <84592352+fabioacarvalho@users.noreply.github.com>
2024-04-21 20:01:49 -0300

    up

---
+++ content/fundamentals/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fundamentals // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade;
+            decimal altura;
+            // string nome;
+
+            idade = 27;
+            Console.WriteLine(idade);
+
+            altura = 1.78m;
+            Console.WriteLine(altura);
+            
+            string email = "teste@teste.com";
+            Console.WriteLine(email);
+        }
+    }
+}
